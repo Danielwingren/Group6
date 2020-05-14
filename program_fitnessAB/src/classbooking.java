8a248bbf61c4d0695be9795a123c0811c049a484
Danne <Danielwingren@live.se>
2020-05-14 18:29:27 +0200

    Har lagt till en hel drös med funktioner: - Jobbat vidare en hel del på main - Lagt till några bilder just for fun och redigerat lite själv :D - Har skapat upp metoder och menyer för att redigera medlemsinfo, mycket är dock fortfarande under konstruktion... - Nästa steg är att lösenordet faktiskt uppdateras när man kör metoden "Change password", gör en ny commit när den lirar! //Dannelito

--- program_fitnessAB/src/classbooking.java
+++ program_fitnessAB/src/classbooking.java
@@ -6,12 +6,23 @@ import java.sql.*;
 import static javax.swing.JOptionPane.showMessageDialog;
 
 public class classbooking {
+    public static final String DB_URL = "jdbc:sqlite:db_fitnessAB.db"; // Sökväg till SQLite-databas. Denna bör nu vara relativ så att den fungerar för oss alla i gruppen!
+    public static final String DRIVER = "org.sqlite.JDBC";
+    static Connection conn = null;
 
     public static void employeeScreen (String namn) {
 
-        JOptionPane.showMessageDialog(null, "this is a test for staffsystem");
-    }
+        JOptionPane.showMessageDialog(null, "Welcome "+namn+".\nWhat operation would you like to perform?\n" +
+                                                                      "(Switch method, enter the letter corresponding to the action performed in the UI");
 
+
+
+    }
+    public static void memberscreen (String namn) {
+        JOptionPane.showMessageDialog(null, "Welcome "+namn+".\nWhat operation would you like to perform?\n" +
+                "(Switch method, enter the letter corresponding to the action performed in the UI");
+
+    }
 
 
 
