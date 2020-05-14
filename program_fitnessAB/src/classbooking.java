32d0f3037f405e9263a769442132f5aa243b70ed
Danielwingren <danielwingren@live.se>
2020-05-14 13:33:42 +0200

    test2

--- program_fitnessAB/src/classbooking.java
+++ program_fitnessAB/src/classbooking.java
@@ -5,10 +5,11 @@ import java.sql.*;
 
 import static javax.swing.JOptionPane.showMessageDialog;
 
-public class staffsystem {
+public class classbooking {
 
     public static void teststaff () {
 
         JOptionPane.showMessageDialog(null, "this is a test for staffsystem");
     }
+
 }
---
+++ program_fitnessAB/src/membershipSystem.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+import org.sqlite.SQLiteConfig;
+import java.sql.*;
+
+import static javax.swing.JOptionPane.showMessageDialog;
+
+public class membershipSystem {
+
+    public static void testmember () { JOptionPane.showMessageDialog(null, "this is a test for membersystem");
+    //metod1()
+        //metod2()
+        //metod3()
+
+
+
+    }
+}
